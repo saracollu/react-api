4c1c623676de74fe202dd947f890bdff6b9b5d2b
saracollu <saracollu.ws@gmail.com>
2026-05-31 21:17:18 +0200

    starting card

--- src/components/ActressCard.jsx
+++ src/components/ActressCard.jsx
@@ -4,9 +4,22 @@ export default function ActressCard() {
 
     const [actressCard, setActressCard] = useState([]);
 
+    function getActress() {
+        fetch("https://lanciweb.github.io/demo/api/actresses/")
+            .then(res => res.json()
+                .then(data => {
+                    setActressCard(data)
+                }))
+    }
     return
     <>
-
+        <div className="container">
+            {
+                actressCard.map(actress => (
+                    <div className="card"></div>
+                ))
+            }
+        </div>
 
     </>
 }
