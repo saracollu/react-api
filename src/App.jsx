7411d5299b54c31eee206ce1a6fcba2bd2d8bc15
saracollu <saracollu.ws@gmail.com>
2026-05-31 21:06:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const [actressList, setActressList] = useState([])
+
+  function getList() {
+    fetch("https://lanciweb.github.io/demo/api/actresses/")
+      .then(res => res.json())
+      .then(data => {
+        setActressList(data)
+      })
+  }
+  getList()
+
+  return (
+    <>
+
+      <ul>
+        {
+          actressList.map(actress => (
+            <li key={actress.id}>{actress.name}</li>
+          ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
