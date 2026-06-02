5965608e48c33c7a2875eae797391ea0d2c56e78
saracollu <saracollu.ws@gmail.com>
2026-06-02 13:02:45 +0200

    cards making

--- src/components/ActressCard.jsx
+++ src/components/ActressCard.jsx
@@ -15,18 +15,23 @@ export default function ActressCard() {
     return (
         <>
             <div className="card-container">
-                {
-                    actressCard.map(actress => (
-                        <div key={actress.id} className="card">
-                            <h2>{actress.name}</h2>
-                            <span>{actress.birth_year}</span>
-                            <span>{actress.nationality}</span>
-                            <span>{actress.biography}</span>
-                            <img src={actress.image} alt="" />
-                            <span>{actress.awards}</span>
-                        </div>
-                    ))
-                }
+
+                <div className="row">
+                    {
+                        actressCard.map(actress => (
+                            <div key={actress.id} className="col">
+                                <div className="card p-5 m-3">
+                                    <h2>{actress.name}</h2>
+                                    <span>{actress.birth_year}</span>
+                                    <span>{actress.nationality}</span>
+                                    <span>{actress.biography}</span>
+                                    <img src={actress.image} alt="" />
+                                    <span>{actress.awards}</span>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
 
         </>
